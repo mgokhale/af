6db36154bc985dabca9efaa11475bc91a3bf700e
Mandar Gokhale <mgokhale@gmail.com>
2015-06-05 22:16:01 -0700

    welcome

--- app.rb
+++ app.rb
@@ -8,12 +8,14 @@ require './environments'
 class Note < ActiveRecord::Base
 end
 
-# get "/" do
-# 	"Hello"
-# end
+get "/welcome" do
+	"welcome"
+end
 
 get "/" do
   @notes = Note.order("created_at DESC")
   @title = "Welcome."
   erb :"notes/index"
 end
+
+
